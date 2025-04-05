594a638c2ed7baba32bd22cbd15e2cd29470c319
Konstantinos Politis <74538281+kostas-politis@users.noreply.github.com>
2025-04-05 22:14:36 +0300

    feat: search bar (#14)

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from '@/components/navigation/Navbar';
 import { LeftSidebarProvider } from '@/components/navigation/left-sidebar/LeftSidebarContext';
 import NavLinks from '@/components/navigation/left-sidebar/NavLinks';
 import NavAuth from '@/components/navigation/left-sidebar/NavAuth';
+import RightSidebar from '@/components/navigation/RightSidebar';
 
 export default function RootLayout({
   children,
@@ -13,12 +14,15 @@ export default function RootLayout({
     <main className='grid grid-cols-[auto_1fr]'>
       <LeftSidebarProvider>
         <Navbar className='sticky top-0 z-50 col-span-2 h-[53px] lg:h-[100px]' />
-        <LeftSidebar className='fixed top-[53px] h-[calc(100vh-53px)] self-start lg:sticky lg:top-[100px] lg:h-[calc(100vh-100px)]'>
+        <LeftSidebar className='fixed top-[53px] z-40 h-[calc(100vh-53px)] self-start lg:sticky lg:top-[100px] lg:h-[calc(100vh-100px)]'>
           <NavLinks />
           <NavAuth />
         </LeftSidebar>
       </LeftSidebarProvider>
-      {children}
+      <div className='col-span-2 flex lg:col-span-1'>
+        <section className='relative w-full p-8'>{children}</section>
+        <RightSidebar className='hidden shrink-0 xl:block' />
+      </div>
     </main>
   );
 }
