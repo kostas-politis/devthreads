31135174b596eb49f60baff057f38dfeec29eca3
Konstantinos Politis <politisk273@gmail.com>
2025-06-23 00:57:08 +0300

    test11

---
+++ apps/api/src/middleware/correlation.ts
@@ -0,0 +1,30 @@
+import { correlator } from "@/util/correlator";
+import { AppError } from "@/util/error";
+import { randomUUID } from "crypto";
+import type { NextFunction, Request, Response } from "express";
+
+const CORRELATION_ID_HEADER = "x-correlation-id";
+
+export function correlation(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
+  let correlationId = req.headers[CORRELATION_ID_HEADER];
+
+  if (!correlationId) {
+    correlationId = randomUUID();
+    req.headers[CORRELATION_ID_HEADER] = correlationId;
+  }
+
+  if (Array.isArray(correlationId) || correlationId.includes(",")) {
+    throw new AppError(
+      `Multiple ${CORRELATION_ID_HEADER} headers not allowed.`,
+      400,
+    );
+  }
+
+  res.setHeader(CORRELATION_ID_HEADER, correlationId);
+
+  correlator.run(next, correlationId);
+}
---
+++ apps/api/src/middleware/error.ts
@@ -0,0 +1,15 @@
+import { errorHandler } from "@/util/error-handler";
+import type { NextFunction, Request, Response } from "express";
+
+export function error(
+  error: unknown,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction,
+): void {
+  const appError = errorHandler.convertError(error);
+  appError.isOperational = true;
+  const { status, message, errors } = errorHandler.handleError(appError);
+  res.status(status).json({ status, message, errors });
+}
